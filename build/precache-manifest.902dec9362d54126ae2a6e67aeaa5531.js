e948a5e3028958910bec8b7c70db68c3c2204128
Aimee Lualhati <aimee.lualhati@g.batstate-u.edu.ph>
2021-11-26 09:19:43 +0800

    commit
    asd

---
+++ build/precache-manifest.902dec9362d54126ae2a6e67aeaa5531.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e26ab859aa94a1c09ca5bab8912353d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3faff811bdd6d4f0c2b0",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "49e30930c87da7ddd3af",
+    "url": "/static/css/main.4da9dee8.chunk.css"
+  },
+  {
+    "revision": "3faff811bdd6d4f0c2b0",
+    "url": "/static/js/2.01758888.chunk.js"
+  },
+  {
+    "revision": "c59e716e54e76b7c9d3f4b689040f30c",
+    "url": "/static/js/2.01758888.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49e30930c87da7ddd3af",
+    "url": "/static/js/main.1826bf86.chunk.js"
+  },
+  {
+    "revision": "60e03ac997a50423e6fc",
+    "url": "/static/js/runtime-main.7c3baa45.js"
+  },
+  {
+    "revision": "2e8d5e9111fcf4f1e3ea5b0d6f4243b1",
+    "url": "/static/media/icon.2e8d5e91.png"
+  }
+]);
---
+++ src/components/Main/Main.js
@@ -0,0 +1,116 @@
+import React from 'react'
+import * as tf from '@tensorflow/tfjs';
+
+export class Main extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            images: "",
+            message: ""
+        }
+        this.predicting = this.predicting.bind(this)
+        this.model = undefined
+      }
+
+      async componentDidMount() {
+        //   localStorage.setItem("model", JSON.stringify(require("./model/model.json")))
+        // const handler = tfn.io.fileSystem([require("./model/model.json"), require("./model/weights.bin")]);
+        this.model = await tf.loadLayersModel("https://calm-harbor-72858.herokuapp.com/model/model.json")
+        // this.model = await tf.loadLayersModel(require("../../assets/model/model.json"))
+      }
+
+    imageHandler = (e) => {
+        e.preventDefault();
+        var file = window.document.querySelector("#rice").files[0];
+        var reader = new FileReader();
+        var url = reader.readAsDataURL(file);
+
+        reader.onloadend = function (e) {
+            this.setState({
+                images: reader.result
+            })
+            }.bind(this);
+    }
+
+    handleWait = () =>{
+        this.setState({message: "Please Wait"})
+    }
+
+    handleResult = (data) => {
+        var max = 0
+        var idx = 0
+        const labels = {
+            0: "healthy",
+            1: "brown spot",
+            2: "hispa",
+            3: "leaf blast",
+			4: "error"
+			
+        }
+        var result = ""
+		var info = ""
+        for(var i = 0; i < data.length; i++) {
+            if(data[i] > max) {
+                max = data[i]
+                idx = [i]
+            }
+        }
+        max = max.toFixed(4)
+        if (idx == 0){
+            result = "Your rice is " + labels[idx] + " (" + max*100 + "%)"
+        }else{
+            result = "Disease: " + labels[idx] + " (" + max*100 + "%)"
+			
+			
+			if(labels[idx] == "leaf blast") {
+				result += " \n a disease of rice caused by the fungus Pyricularia oryae, characterized by elliptical leaf spots with reddish-brown margins, brownish lesions and neck rot of the fruiting panicles, and stunting of the plant.";
+			}
+			if(labels[idx] == "brown spot") {
+				result += " \n Brown spot is caused by the fungus Cochliobolus miyabeanus. Also called Helminthosporium leaf spot, it is one of the most prevalent rice diseases in Louisiana. When C. miyabeanus attacks the rice plants at emergence, the resulting seedling blight causes sparse or inadequate stands and weakened plants";
+			}
+			if(labels[idx] == "hispa") {
+				result += " \n Rice hispa scrapes the upper surface of leaf blades leaving only the lower epidermis. It also tunnels through the leaf tissues. When damage is severe, plants become less vigorous.";
+			}
+			if(labels[idx] == "error") {
+				result = "ERROR: No Classification " + labels[idx] ;
+			}
+			
+        }
+
+
+        this.setState({message: result})
+    }
+
+    async predicting(){
+        this.handleWait()
+
+        const image = window.document.getElementsByTagName('img')[1]
+        const img = tf.browser.fromPixels(image).resizeBilinear([224,224], false).mul(1./255.).expandDims(0)
+        const prediction = this.model.predict(img);
+        const result = await prediction.data()
+
+        this.handleResult(result)
+    }
+    
+    render(){
+        return (
+		
+            <div className="content">
+			
+                <div className="item" >
+                    <img className="icon" src={require("../../assets/icon.png")} />
+                </div>
+                <div className="item">
+                    <h4>Select Your Image</h4>
+                </div>
+                <input required ref="file" type="file" name="images" accept="image/*" className="item upload" id="rice"   onChange={this.imageHandler}/>
+				<div className="item btn" onClick={this.predicting}>Detect Rice Disease!</div>
+                <div>{this.state.message}</div>
+                <div>
+                    <img src={this.state.images} className="item image"/>
+                </div>
+            </div>
+        );
+    }
+}
+// export default Main
